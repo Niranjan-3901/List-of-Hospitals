339d2eabb90759eb188eb43b1f7f6936df0ca0e5
Niranjan-3901 <niranjankumarbarai456@gmail.com>
2024-05-10 08:18:54 +0530

    adding Option Block and handling it's buttons

--- script.js
+++ script.js
@@ -134,6 +134,10 @@ function createHospitalCard(hName, container) {
   }
   Name.textContent = thename;
   newCard.title = thename;
+  className = thename.split(" ").join("_")
+  // console.log(className)
+  newCard.classList.add(`${className}`)
+  // console.log(newCard.classList)
   Addr = capitalize(Hospital_details["ADDRESS"]);
   city = Hospital_details["CITY"].toUpperCase();
   pinCode = Hospital_details["PIN CODE"];
@@ -215,23 +219,107 @@ window.addEventListener("resize", () => {
   }
 });
 
-document.addEventListener("mouseover", (event) => {
-  const targetElement = event.target;
-  if (targetElement.name) {
-    targetElement.title = targetElement.name;
-  } else if (targetElement.id) {
-    targetElement.title = targetElement.id;
-  }
-});
+// document.addEventListener("mouseover", (event) => {
+//   const targetElement = event.target;
+//   if (targetElement.name) {
+//     targetElement.title = targetElement.name;
+//   } else if (targetElement.id) {
+//     targetElement.title = targetElement.id;
+//   }
+// });
 
 document.addEventListener("click", (event) => {
+  //menu open and close handling
   if (event.target.closest(".menuButton")) {
     menuHandle();
+  }
+  //search box visibility handling
+  if (event.target.closest(".label_of_search")) makeSearchBoxVisible();
+
+  //handling single click event when double click already done and for closing double clicked option box
+  if (document.body.dataset.is_dbl_clicked == "true") {
+    let grid = document.getElementById("grid")
+    if (grid.querySelector(".card .option-block")) {
+      grid.querySelector(".card .option-block").closest(".card").removeChild(document.querySelector(".option-block"))
+      document.body.dataset.is_dbl_clicked="false"
+    }
   }
 });
 
 document.addEventListener("DOMContentLoaded", fetchMainData());
 
-document.addEventListener("click",(event)=>{
-  if(event.target.closest(".label_of_search")) makeSearchBoxVisible();
-})
+document.addEventListener("dblclick", (event) => {
+  const clickedCard = event.target.closest(".card");
+  if(clickedCard){
+  let className = clickedCard.classList[1];}
+  if (clickedCard && document.body.dataset.is_dbl_clicked == "false" && clickedCard.closest(".grid")) {
+    addDoubleClickBlock(event, clickedCard, className)
+  }
+  else if(document.body.dataset.is_dbl_clicked == "true") {
+    clickedCard.removeChild(document.getElementById("option-block"))
+    document.body.dataset.is_dbl_clicked = "false"
+    addDoubleClickBlock(event, clickedCard, className)
+  }
+});
+
+const addDoubleClickBlock = (event, clickedCard) => {
+  const block = document.createElement("span");
+  block.classList.add("option-block");
+  block.id = "option-block";
+  const deleteDiv = document.createElement("div")
+  const pinDiv = document.createElement("div")
+  deleteDiv.textContent = "Delete";
+  if(clickedCard.classList[2]){
+    pinDiv.textContent = "Unpin"
+  }else{
+    pinDiv.textContent = "Pin";
+  }
+  deleteDiv.onclick = () => {
+    let grid = document.getElementById("grid");
+    grid.removeChild(clickedCard)
+    document.body.dataset.is_dbl_clicked = 'false'
+  }
+  pinDiv.onclick = pinClickHandle(pinDiv,grid,clickedCard)
+  document.body.dataset.is_dbl_clicked = "true"
+  block.appendChild(deleteDiv);
+  block.appendChild(pinDiv);
+  block.style.position = "absolute";
+
+  const cardRect = clickedCard.getBoundingClientRect();
+  const offsetX = event.clientX - cardRect.left;
+  const offsetY = event.clientY - cardRect.top;
+
+  block.style.left = `${offsetX}px`;
+  block.style.top = `${offsetY}px`;
+  clickedCard.appendChild(block);
+}
+
+function pinClickHandle(pinDiv, grid, clickedCard) {
+  if (pinDiv.textContent === "Pin") {
+    return function () {
+      const firstChild = grid.firstChild;
+      grid.insertBefore(clickedCard, firstChild);
+      clickedCard.classList.add("pinned");
+      const pinnedSVG = document.createElement("div");
+      pinnedSVG.id = "pinnedCardSVG";
+      pinnedSVG.style.position="absolute";
+      pinnedSVG.style.width='18px';
+      pinnedSVG.style.height='18px';
+      pinnedSVG.style.zIndex="999";
+      pinnedSVG.style.left = "6px";
+      pinnedSVG.style.top = `6px`;
+      pinnedSVG.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pin-angle-fill" viewBox="0 0 16 16"><path d="M9.828.722a.5.5 0 0 1 .354.146l4.95 4.95a.5.5 0 0 1 0 .707c-.48.48-1.072.588-1.503.588-.177 0-.335-.018-.46-.039l-3.134 3.134a6 6 0 0 1 .16 1.013c.046.702-.032 1.687-.72 2.375a.5.5 0 0 1-.707 0l-2.829-2.828-3.182 3.182c-.195.195-1.219.902-1.414.707s.512-1.22.707-1.414l3.182-3.182-2.828-2.829a.5.5 0 0 1 0-.707c.688-.688 1.673-.767 2.375-.72a6 6 0 0 1 1.013.16l3.134-3.133a3 3 0 0 1-.04-.461c0-.43.108-1.022.589-1.503a.5.5 0 0 1 .353-.146"/></svg>'
+      clickedCard.appendChild(pinnedSVG);
+      pinDiv.textContent = "Unpin";
+    };
+  } else {
+    return function () {
+      clickedCard.classList.remove("pinned");
+      const pinnedSVG = clickedCard.querySelector("#pinnedCardSVG");
+      if (pinnedSVG) {
+        pinnedSVG.remove();
+      }
+      pinDiv.textContent = "Pin";
+    };
+  }
+}
